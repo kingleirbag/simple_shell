432dedeef51863ce7d1eb9f9c60316965cfaebc0
Cosbyk <mwathikuria@gmail.com>
2023-04-25 02:56:20 -0700

    prompt commit

---
+++ prompt.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * prompt - Displays the prompt "cisfun$$ ".
+ *
+ * Return: User input string.
+ */
+char *prompt(void)
+{
+char *print_line = NULL;
+size_t character = 0;
+
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "cisfun$ ", 8);
+
+if (getline(&print_line, &character, stdin) == -1)
+{
+free(print_line);
+return (NULL);
+}
+
+return (print_line);
+}
