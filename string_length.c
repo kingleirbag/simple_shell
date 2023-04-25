f3fdcda0ec11de1ecd0f26c752d6a9189fceec54
Cosbyk <mwathikuria@gmail.com>
2023-04-25 03:18:03 -0700

    string_length commit

---
+++ string_length.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_length - Takes a pointer to a string
+ * and returns the length of string as integer value.
+ * @str:  Pointer to a character array.
+ *
+ * Return: value of the counter (length).
+ */
+int string_length(char *str)
+{
+
+	int length = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+	return (length);
+}
