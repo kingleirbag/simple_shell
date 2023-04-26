12c91a5f1e591d8a89a5385182bc7009711859f4
Cosby Kuria <122878559+Cosbyk@users.noreply.github.com>
2023-04-26 12:30:16 +0300

    Update string_length.c

--- string_length.c
+++ string_length.c
@@ -3,7 +3,9 @@
 /**
  * string_length - Takes a pointer to a string
  * and returns the length of string as integer value.
+ *
  * @str:  Pointer to a character array.
+ *
  * Return: value of the counter (length).
  */
 int string_length(char *str)
